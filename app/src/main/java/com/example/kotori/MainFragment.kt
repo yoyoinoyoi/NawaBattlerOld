f0b57bc04d586e8fdff5844d2b1078f84b709f38
yoyoinoyoi <y.mar.97p25@gmail.com>
2022-12-09 22:00:33 +0900

    [change] テーマカラーの変更
    アプリ全体の背景色を水色に変更しました

--- app/src/main/java/com/example/kotori/MainFragment.kt
+++ app/src/main/java/com/example/kotori/MainFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.kotori.databinding.FragmentMainBinding
+import com.nanaten.customdialog.CustomDialog
 
 class MainFragment : Fragment() {
 
@@ -27,6 +28,15 @@ class MainFragment : Fragment() {
             findNavController().navigate(action)
         }
 
+        // バトル準備画面に遷移
+        binding.galleryButton.setOnClickListener {
+            CustomDialog.Builder(this)
+                .setTitle("カスタムタイトル")
+                .setMessage("カスタムメッセージ")
+                .setPositiveButton("はい") {  }
+                .build()
+                .show(childFragmentManager, CustomDialog::class.simpleName)
+        }
         return view
     }
 
@@ -35,6 +45,4 @@ class MainFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
-
-
 }
